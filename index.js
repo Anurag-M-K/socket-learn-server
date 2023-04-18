e8f0615083a8700692e6b44768df2571557bf851
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2023-04-19 00:07:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const {Server} = require("socket.io");
+const server = http.createServer(app);
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+ 
+//INIITIATE SOCKET CONNECTION BASED ON EVENTS ,io.on - listening
+io.on("connection",(socket)=>{
+    console.log("user connected ",socket.id);
+
+    socket.on("join_room",(data)=>{ //recieving data from frontend by events
+      socket.join(data)
+      console.log('user with id : ',socket.id +  "joined room : ",data)
+    })
+
+
+    socket.on("send_message",(data)=>{ 
+      socket.to(data.room).emit("recieve_message",data) //sending message to the room which we included
+    })
+    socket.on("disconnect",()=>{
+      console.log('user diconnected', socket.id)
+    })
+})
+
+
+
+server.listen(8080,()=>{
+  console.log('server running at 5000')
+})
